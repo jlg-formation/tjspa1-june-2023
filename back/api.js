d9e4a427a9d404a7548fda3d50267baadaa884d2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-28 15:01:12 +0200

    dev: added api

--- back/api.js
+++ back/api.js
@@ -1,1 +1,14 @@
-module.exports = 123
+const express = require("express");
+
+const app = express.Router();
+
+const articles = [
+  { id: "a1", name: "Tournevis", price: 2.99, qty: 450 },
+  { id: "a2", name: "Pelle", price: 5.5, qty: 52 },
+];
+
+app.get("/articles", (req, res) => {
+  res.json(articles);
+});
+
+module.exports = app;
--- back/server.js
+++ back/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const serveIndex = require("serve-index");
+const api = require("./api");
+
 const app = express();
 
 const publicDir = ".";
@@ -8,6 +10,8 @@ app.use((req, res, next) => {
   console.log("req: ", req.url, req.method);
   next();
 });
+
+app.use("/api", api);
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
