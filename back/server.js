569d37cfe11a2681aba6d3c6da9cbe703ddae62a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-28 12:17:52 +0200

    dev: added static server

--- back/server.js
+++ back/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const serveIndex = require('serve-index')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const publicDir = '.'
+
+app.use(express.static(publicDir))
+app.use(serveIndex(publicDir, {icons: true}))
 
 app.listen(3000, ()=>{
   console.log('Server started on port 3000');
