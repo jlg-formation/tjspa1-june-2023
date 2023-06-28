24ede4a9b02e34c0848b4332b2327af93b9f4f6a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-28 14:12:11 +0200

    dev: prettier

--- back/server.js
+++ back/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const serveIndex = require('serve-index')
-const app = express()
+const express = require("express");
+const serveIndex = require("serve-index");
+const app = express();
 
-const publicDir = '.'
+const publicDir = ".";
 
-app.use((req, res, next)=>{
-  console.log('req: ', req.url, req.method);
-  next()
-})
+app.use((req, res, next) => {
+  console.log("req: ", req.url, req.method);
+  next();
+});
 
-app.use(express.static(publicDir))
-app.use(serveIndex(publicDir, {icons: true}))
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
-app.listen(3000, ()=>{
-  console.log('Server started on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
