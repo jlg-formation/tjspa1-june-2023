2d1a18473441d8ec1cad5f863dce70fbf3ac3095
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-29 16:39:32 +0200

    dev: added dom and css addroute

--- front/src/stock/routes/AddRoute.tsx
+++ front/src/stock/routes/AddRoute.tsx
@@ -4,6 +4,22 @@ export default function AddRoute() {
   return (
     <main className="AddRoute">
       <h1>Ajout d'un article</h1>
+      <form>
+        <label>
+          <span>Nom</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Prix</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Quantité</span>
+          <input type="text" />
+        </label>
+
+        <button className="primary">Ajouter</button>
+      </form>
     </main>
   );
 }
