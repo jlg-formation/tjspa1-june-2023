c7e184d13728ca6c73239a18f407604e58041c82
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-29 15:37:26 +0200

    dev: added link

--- front/src/routes/HomeRoute.tsx
+++ front/src/routes/HomeRoute.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import "./HomeRoute.scss";
 
 export default function HomeRoute() {
   return (
     <main className="HomeRoute">
       <h1>Gérer efficacement votre stock !</h1>
-      <a href="#" className="button primary">
+      <Link to="/stock" className="button primary">
         Voir le stock
-      </a>
+      </Link>
     </main>
   );
 }
